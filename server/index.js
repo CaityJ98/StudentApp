06e65cbf06273a7662b2f8763273ca120edc544b
CaityJ98 <caitlynjschaper@gmail.com>
2022-08-06 19:44:40 +1000

    bug fix

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ connectDB();
 
 app.use(routes);
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging'){
-  app.use(express.static(path.join(_dirname, 'build'));
+  app.use(express.static(path.join(_dirname, 'build')));
   app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
